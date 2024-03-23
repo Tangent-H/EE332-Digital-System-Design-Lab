e23be719ad523ea675b4b89819e9a382d0713575
Tangent-H <12112619@mail.sustech.edu.cn>
2024-03-23 21:38:47 +0800

    Lab2_priority_encoder: refactor

--- Lab2/priority_encoder/src/tb/tb_prior_enc.vhd
+++ Lab2/priority_encoder/src/tb/tb_prior_enc.vhd
@@ -9,37 +9,30 @@ end tb_prior_enc ;
 architecture test of tb_prior_enc is
     component top_module is
         port (
-            en : in std_logic;
             S: in std_logic_vector(15 downto 0);
-            Z1 : out std_logic_vector (3 downto 0);
-            r1 : out std_logic;
-            ys1 : out std_logic;
-            Z2 : out std_logic_vector (3 downto 0);
-            r2 : out std_logic;
-            ys2 : out std_logic
+            Z : out std_logic_vector (3 downto 0);
+            r : out std_logic
         );
     end component ;
 
 
     signal S_tb : std_logic_vector(15 downto 0) ;
-    signal Z1_tb : std_logic_vector(3 downto 0) ;
-    signal r1_tb : std_logic ;
-    signal Z2_tb : std_logic_vector(3 downto 0) ;
-    signal r2_tb : std_logic ;
+    signal Z_tb : std_logic_vector(3 downto 0) ;
+    signal r_tb : std_logic ;
 begin
-    UUT: top_module port map(en => '1', S => S_tb, Z1 => Z1_tb, r1 => r1_tb, ys1 => open, Z2 => Z2_tb, r2 => r2_tb, ys2 => open) ;
+    UUT: top_module port map(S => S_tb, Z => Z_tb, r => r_tb) ;
     
     seq: process
     begin
         S_tb <= "0000000000000000" ;
-        wait for 5 ns ;
+        wait for 10 ns ;
         
         S_tb <= "0000000000000001" ;
-        wait for 5 ns ;
+        wait for 10 ns ;
         
         for i in 0 to 15 loop
             S_tb <=to_stdlogicvector(to_bitvector(S_tb) sll 1);
-            wait for 5 ns ;
+            wait for 10 ns ;
         end loop ;
         
         wait;
