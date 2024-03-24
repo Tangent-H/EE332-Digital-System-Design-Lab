b5b32675b3b097b4e7cfdcf1f956712b7957649a
Tangent-H <12112619@mail.sustech.edu.cn>
2024-03-24 11:28:47 +0800

    Lab2_priority_encoder: no 4-2mux version

--- Lab2/priority_encoder/src/hdl/prior_tree.vhd
+++ Lab2/priority_encoder/src/hdl/prior_tree.vhd
@@ -18,7 +18,7 @@ component prior_enc42 is
     );
 end component;
 signal h_enc_in : std_logic_vector(3 downto 0);
-signal l_enc3_out, l_enc2_out, l_enc1_out, l_enc0_out, h_enc_out: std_logic_vector(1 downto 0);
+signal l_enc3_out, l_enc2_out, l_enc1_out, l_enc0_out, h_enc_out, l_enc_out: std_logic_vector(1 downto 0);
 begin
     l_enc3: prior_enc42 port map (S42 => S(15 downto 12), Z42 => l_enc3_out, r42 => h_enc_in(3));
     l_enc2: prior_enc42 port map (S42 => S(11 downto 8), Z42 => l_enc2_out, r42 => h_enc_in(2));
@@ -28,9 +28,10 @@ begin
     
     Z(3 downto 2) <= h_enc_out;
     with h_enc_out select
-        Z(1 downto 0) <= l_enc3_out when "11",
+        l_enc_out <= l_enc3_out when "11",
                           l_enc2_out when "10",
                           l_enc1_out when "01",
                           l_enc0_out when "00",
                           "00" when others;
+    Z(1 downto 0) <= l_enc_out; -- I tried not using l_enc_out and directly assigning Z(1 downto 0) in the 4-2multiplexer, but it introduces an additional register
 end architecture;
--- Lab2/priority_encoder/src/hdl/top_module.vhd
+++ Lab2/priority_encoder/src/hdl/top_module.vhd
@@ -11,22 +11,22 @@ entity top_module is
 end top_module;
 
 architecture behavioral of top_module is
-    -- component prior_tree
-    --     port (
-    --         S: in std_logic_vector(15 downto 0);
-    --         Z : out std_logic_vector (3 downto 0);
-    --         r : out std_logic
-    --     );
-    -- end component;
-
-    component prior_enc
+    component prior_tree
         port (
             S: in std_logic_vector(15 downto 0);
             Z : out std_logic_vector (3 downto 0);
             r : out std_logic
         );
     end component;
+
+    -- component prior_enc
+    --     port (
+    --         S: in std_logic_vector(15 downto 0);
+    --         Z : out std_logic_vector (3 downto 0);
+    --         r : out std_logic
+    --     );
+    -- end component;
 begin
-    -- prior_tree_t: prior_tree port map (S, Z, r);
-    prior_enc_t: prior_enc port map (S, Z, r);
+    prior_tree_t: prior_tree port map (S, Z, r);
+    -- prior_enc_t: prior_enc port map (S, Z, r);
 end architecture;
